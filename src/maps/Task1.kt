2f7646ac18d53325d23be230ca5154e2c241c705
Viktor Shterev <viktor.shterev55@gmail.com>
2025-07-15 15:18:52 +0300

    kotlin maps

---
+++ src/maps/Task1.kt
@@ -0,0 +1,8 @@
+package maps
+
+fun main() {
+
+    var codeysMap = mapOf("valley" to "scorpions", "creek" to "snakes", "forest" to "bears")
+
+    println(codeysMap)
+}
---
+++ src/maps/Task4.kt
@@ -0,0 +1,13 @@
+package maps
+
+fun main() {
+
+    var momaPaintings = mutableMapOf("Les Demoiselles d'avignon" to "Pablo Picasso", "The Starry Night" to "Vincent Van Gogh", "Mona Lisa" to "Leonardo Da Vinci", "The Persistance of Memory" to "Salvador Dali")
+
+    if (!momaPaintings.containsValue("Claude Monet")) {
+        momaPaintings.put("Water Lillies", "Claude Monet")
+    }
+
+    momaPaintings.remove("Mona Lisa")
+    println(momaPaintings)
+}
